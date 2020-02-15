9ecfa637a8f3fdd9507f2b67704ed7d66bf918b7
l03m03@SURCO.COM <l03m03@SURCO.COM>
2020-02-14 21:49:55 -0500

    java

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -7,8 +7,10 @@ package javaapplication2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 
@@ -25,6 +27,7 @@ public class JavaApplication2 {
             Statement statement = connect.createStatement();
             String _query = "SELECT * FROM producto";
             ResultSet resultSet = statement.executeQuery(_query);
+            String format="|%1$-3d|%2$-17s|%3$-5d\n";
             while (resultSet.next()) {
                 int idProducto = resultSet.getInt("id_producto");
                 String descProducto = resultSet.getString("desc_producto");
@@ -34,8 +37,68 @@ public class JavaApplication2 {
                 System.out.println("Precio: " + precio);
                 System.out.println("=============================");
             }
+            Scanner scan = new Scanner(System.in);
+            System.out.println("¿Que deseas hacer: INSERTAR/ BORRAR/ CONSULTAR / ACTUALIZAR");
+            String accion = scan.nextLine();
+            if(accion.equals("INSERTAR")){
+                scan = new Scanner(System.in);
+                System.out.println("Ingresar el id_producto");
+                String idprod = scan.nextLine();
+                
+                scan = new Scanner(System.in);
+                System.out.println("Ingresa el edsc_producto");
+                String descprod = scan.nextLine();
+                
+                scan= new Scanner(System.in);
+                System.out.println("Ingresa el precio");
+                String precio=scan.nextLine();
+                _query = "INSERT INTO producto VALUES(?,?,?)";
+                PreparedStatement ps = connect.prepareStatement(_query);
+                ps.setInt(1,Integer.parseInt(idprod));
+                ps.setString(2,descprod);
+                ps.setInt(3,Integer.parseInt(precio));
+                ps.executeUpdate();
+                                
+                
+                
+            }
+            else if (accion.equals("BORRAR")){
+                        scan=new Scanner(System.in);
+                        System.out.println("Ingresa el id_producto");
+                        String idprod=scan.nextLine();
+                _query = "DELETE FROM producto WHERE id_producto = ?";
+                PreparedStatement ps = connect.prepareStatement(_query);
+                ps.setInt(1, Integer.parseInt(idprod));
+                ps.executeUpdate();
+           }
+            else if (accion.equals("ACTUALIZAR")) {
+                scan = new Scanner(System.in);
+                System.out.println("Ingresa el id_producto");
+                String idprod = scan.nextLine();
+                
+                scan = new Scanner(System.in);
+                System.out.println("Ingresa el desc_producto");
+                String descprod = scan.nextLine();
+                
+                scan = new Scanner(System.in);
+                System.out.println("Ingresa el precio");
+                String precio = scan.nextLine();
+                
+                _query = "UPDATE producto SET desc_producto = ?, precio = ? WHERE id_producto = ?";
+                PreparedStatement ps = connect.prepareStatement(_query);
+                ps.setString(1, descprod);
+                ps.setInt(2, Integer.parseInt(precio));
+                ps.setInt(3, Integer.parseInt(idprod));
+                ps.executeUpdate();
+            }
+            resultSet.close();
+            statement.close();
+            connect.close();
+
+           
+       
         } catch (Exception e) {
-            //
+            System.err.println(e);
         }
 
       
